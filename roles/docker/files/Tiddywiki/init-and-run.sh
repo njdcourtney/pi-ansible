b3e24ef0f1d6a51ce58e76ed9193e9edb01bf130
Nathan Courtney <nathan.courtney@gmail.com>
2020-01-21 22:09:06 +0000

    Large refactor of Tiddywiki into webservices

--- roles/docker/files/Tiddywiki/init-and-run.sh
+++ roles/docker/files/Tiddywiki/init-and-run.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+# Init the service directory if required
 if [ ! -d "$WIKI_NAME" ]
 then
   tiddlywiki "$WIKI_NAME" --init server
 fi
 
-exec tiddlywiki "$WIKI_NAME" --listen "readers=$WIKI_READERS" "writers=$WIKI_WRITERS" "credentials=$WIKI_CREDENTIALS" "port=$WIKI_PORT" host=0.0.0.0
+# Set-up the wiki path variable if needed
+if [ "$WIKI_PATH_PREFIX" != '/' ]
+then
+   mkdir "$WIKI_NAME"/tiddlers
+   echo -e "title: \$:/config/tiddlyweb/host\n\n\$protocol$//\$host\$$WIKI_PATH_PREFIX/" > "$WIKI_NAME"/tiddlers/'$__config_tiddlyweb_host.tid'
+fi
+
+# Start the Wiki server
+exec tiddlywiki "$WIKI_NAME" --listen "readers=$WIKI_READERS" "writers=$WIKI_WRITERS" "credentials=$WIKI_CREDENTIALS" "port=$WIKI_PORT" "pathprefix=$WIKI_PATH_PREFIX" host=0.0.0.0
